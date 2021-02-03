00994b0b617189e8cdee573f57199faa8eb6973f
Felix Sotelo <felix.sotelo@wom.cl>
2021-02-02 21:49:40 -0300

    Juegos de Dados

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,50 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBAction func btnRollPressed(_ sender: UIButton) {
+        let diceArray =  [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        var randomDiceOne :[UIImage] = []
+        var randomDiceTwo :[UIImage] = []
+        //random item from the array
+        for _ in 1...6 {
+           
+            randomDiceOne.append(diceArray.randomElement()!)
+            randomDiceTwo.append(diceArray.randomElement()!)
+            
+        }
+        
+        diceImageViewTwo.animationImages = randomDiceTwo
+        diceImageViewOne.animationImages = randomDiceOne
+        
+        diceImageViewTwo.animationDuration = 3.0
+        diceImageViewOne.animationDuration = 3.0
+        
+        diceImageViewTwo.animationRepeatCount = 1
+        diceImageViewOne.animationRepeatCount = 1
+        
+        diceImageViewOne.startAnimating()
+        diceImageViewTwo.startAnimating()
+        
+        diceImageViewOne.image = randomDiceOne.randomElement()
+        diceImageViewTwo.image = randomDiceTwo.randomElement()
+        
+        
+       
+        
+   
+       
+        
+        
+      
+        
+        
     }
-
-
+    
 }
 
